eecaf7673e2c393e64767925a7a2fc5361692736
tluong123 <thomas.luong.2000@gmail.com>
2023-10-31 21:01:46 +1100

    map script

--- Assets/Scripts/LevelMapController.cs
+++ Assets/Scripts/LevelMapController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
+
 public class LevelMapController : MonoBehaviour
 {
     public GameObject[] tiles; // An array of prefabs for different tile types
     public Vector2 tileSize = new Vector2(0.1f, 0.1f); // Size of each tile
+
+
 
 int[,] levelMap =
  {
@@ -29,31 +32,144 @@ int[,] levelMap =
         GenerateLevel();
     }
 
-    void GenerateLevel()
+void GenerateLevel()
+{
+    int numRows = levelMap.GetLength(0);
+    int numColumns = levelMap.GetLength(1);
+
+    for (int row = 0; row < numRows; row++)
     {
-        int numRows = levelMap.GetLength(0);
-        int numColumns = levelMap.GetLength(1);
-
-        // Iterate through the levelMap array and create game objects based on values
-        for (int row = 0; row < numRows; row++)
+        for (int col = 0; col < numColumns; col++)
         {
-            for (int col = 0; col < numColumns; col++)
+            int tileValue = levelMap[row, col];
+
+            // Ensure the tileValue is within the range of the 'tiles' array
+            if (tileValue >= 0 && tileValue < tiles.Length)
             {
-                int tileValue = levelMap[row, col];
+                Vector3 tilePosition = new Vector3(col * tileSize.x, -row * tileSize.y, 0f);
+                GameObject tilePrefab = tiles[tileValue];
 
-                // Ensure the tileValue corresponds to a valid tile prefab
-                if (tileValue >= 0 && tileValue < tiles.Length)
-                {
-                    // Calculate the position for the tile based on row and column
-                    Vector3 tilePosition = new Vector3(col * tileSize.x, -row * tileSize.y, 0f);
+                // Instantiate the prefab at the calculated position
+                GameObject tile = Instantiate(tilePrefab, tilePosition, Quaternion.identity);
 
-                    // Instantiate the appropriate tile prefab
-                    GameObject tile = Instantiate(tiles[tileValue], tilePosition, Quaternion.identity);
+                // Optionally, you can parent the tile to a container for organization
+                // tile.transform.parent = transform;
 
-                    // Optionally, you can parent the tile to a container for organization
-                    // tile.transform.parent = transform;
+                // Check if you want to rotate specific tiles
+                if (shouldRotateTile90(row, col)) {
+                    tile.transform.rotation = Quaternion.Euler(0, 0, 90); // Rotate by 90 degrees
+                }
+                if (shouldRotateTile270(row, col)) {
+                    tile.transform.rotation = Quaternion.Euler(0, 0, 270); // Rotate by 90 degrees
+                }
+                if (shouldRotateTile180(row, col)) {
+                    tile.transform.rotation = Quaternion.Euler(0, 0, 180); // Rotate by 90 degrees
                 }
             }
         }
     }
+}
+bool shouldRotateTile180(int row, int col) {
+    // Check if the tile at this row and column should be rotated
+    // You can implement your custom logic here, such as checking specific rows and columns
+    if ((row == 4 && col == 5) ||
+(row == 3 && col == 11) ||
+(row == 1 && col == 13) ||
+(row == 2 && col == 13) ||
+(row == 3 && col == 13) ||
+(row == 4 && col == 11) ||
+(row == 7 && col == 5) ||
+(row == 8 && col == 8) ||
+(row == 7 && col == 8) ||
+(row == 10 && col == 5) ||
+(row == 11 && col == 5) ||
+(row == 12 && col == 5) ||
+(row == 13 && col == 5) ||
+(row == 13 && col == 8) ||
+(row == 10 && col == 11) ||
+(row == 3 && col == 5))
+{
+        return true; // Rotate the tile
+    }
+    return false; // Do not rotate the tile
+}
+bool shouldRotateTile270(int row, int col) {
+    // Check if the tile at this row and column should be rotated
+    // You can implement your custom logic here, such as checking specific rows and columns
+    if ((row == 2 && col == 3) ||
+(row == 2 && col == 5) ||
+(row == 2 && col == 8) ||
+(row == 2 && col == 9) ||
+(row == 2 && col == 10) ||
+(row == 2 && col == 11) ||
+(row == 6 && col == 3) ||
+(row == 6 && col == 4) ||
+(row == 6 && col == 5) ||
+(row == 6 && col == 8) ||
+(row == 6 && col == 11) ||
+(row == 6 && col == 12) ||
+(row == 6 && col == 13) ||
+(row == 7 && col == 13) ||
+(row == 9 && col == 1) ||
+(row == 9 && col == 2) ||
+(row == 9 && col == 3) ||
+(row == 9 && col == 4) ||
+(row == 9 && col == 5) ||
+(row == 12 && col == 11) ||
+(row == 12 && col == 12) ||
+(row == 10 && col == 10) ||
+(row == 10 && col == 9) ||
+(row == 9 && col == 11) ||
+(row == 2 && col == 4)
+)
+{
+        return true; // Rotate the tile
+    }
+    return false; // Do not rotate the tile
+}
+bool shouldRotateTile90(int row, int col) {
+    // Check if the tile at this row and column should be rotated
+    // You can implement your custom logic here, such as checking specific rows and columns
+    if ((row == 0 && col == 1) ||
+(row == 0 && col == 2) || 
+(row == 0 && col == 3) || 
+(row == 0 && col == 4) || 
+(row == 0 && col == 5) || 
+(row == 0 && col == 6) || 
+(row == 0 && col == 7) ||
+(row == 0 && col == 8) ||
+(row == 0 && col == 9) ||
+(row == 0 && col == 10) ||
+(row == 0 && col == 11) ||
+(row == 0 && col == 12) ||
+(row == 4 && col == 3) ||
+(row == 4 && col == 4) ||
+(row == 4 && col == 7) ||
+(row == 4 && col == 8) ||
+(row == 4 && col == 9) ||
+(row == 4 && col == 10) ||
+(row == 4 && col == 13) ||
+(row == 7 && col == 2) ||
+(row == 7 && col == 3) ||
+(row == 7 && col == 4) ||
+(row == 9 && col == 8) ||
+(row == 7 && col == 10) ||
+(row == 7 && col == 11) ||
+(row == 7 && col == 12) ||
+(row == 9 && col == 0) ||
+(row == 13 && col == 0) ||
+(row == 13 && col == 1) ||
+(row == 13 && col == 2) ||
+(row == 13 && col == 3) ||
+(row == 13 && col == 4) ||
+(row == 13 && col == 7) ||
+(row == 9 && col == 10) ||
+(row == 9 && col == 9) ||
+(row == 10 && col == 13) ||
+(row == 4 && col == 2))
+{
+        return true; // Rotate the tile
+    }
+    return false; // Do not rotate the tile
+}
 }
