06745c0a3f79b6d3c773d9bbd8c527913b72c8ab
tluong123 <thomas.luong.2000@gmail.com>
2023-11-03 17:02:29 +1100

    Aspect Ratio

--- Assets/Scripts/AspectRatioHandler.cs
+++ Assets/Scripts/AspectRatioHandler.cs
@@ -1,27 +1,48 @@
 using UnityEngine;
 
-public class AspectRatioHandler : MonoBehaviour
+public class AspectRatioScaler : MonoBehaviour
 {
-    [SerializeField] private GameObject sixteenByNineElements;
-    [SerializeField] private GameObject fourByThreeElements;
+    private Camera mainCamera;
+    private float baseWidth16_9 = 16f;
+    private float baseHeight16_9 = 9f;
+    private float baseWidth4_3 = 4f;
+    private float baseHeight4_3 = 3f;
 
     void Start()
     {
-        float screenAspect = (float)Screen.width / (float)Screen.height;
+        mainCamera = Camera.main;
 
-        if (Mathf.Approximately(screenAspect, 16.0f / 9.0f))
+        // Calculate the screen's aspect ratio.
+        float screenAspect = (float)Screen.width / Screen.height;
+
+        // Calculate the desired aspect ratios.
+        float targetAspect16_9 = baseWidth16_9 / baseHeight16_9;
+        float targetAspect4_3 = baseWidth4_3 / baseHeight4_3;
+
+        // Calculate the horizontal and vertical scaling factors.
+        float scaleWidth = screenAspect / targetAspect16_9;
+        float scaleHeight = 1f;
+
+        if (screenAspect < targetAspect16_9)
         {
-            sixteenByNineElements.SetActive(true);
-            fourByThreeElements.SetActive(false);
+            // Use 16:9-based scaling if the screen is narrower.
+            mainCamera.orthographicSize /= scaleWidth;
+
+            // Adjust the positions and sizes of your game objects for 16:9 here.
         }
-        else if (Mathf.Approximately(screenAspect, 4.0f / 3.0f))
+        else if (screenAspect > targetAspect4_3)
         {
-            sixteenByNineElements.SetActive(false);
-            fourByThreeElements.SetActive(true);
+            // Use 4:3-based scaling if the screen is wider.
+            mainCamera.orthographicSize /= scaleHeight;
+
+            // Adjust the positions and sizes of your game objects for 4:3 here.
         }
         else
         {
-            // Handle other aspect ratios or provide fallback UI elements.
+            // Screen is between 16:9 and 4:3, no need to scale the camera.
+
+            // Adjust the positions and sizes of your game objects for other aspect ratios here.
         }
     }
 }
+	
